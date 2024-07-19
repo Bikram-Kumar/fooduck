391685f1da95a2c0c6beac4d33478c9de97f03b1
Bikram Kumar <coolbikram0@gmail.com>
2024-07-19 12:55:21 +0530

    splash screen after start

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooduck/managers/food_category_manager.dart';
 import 'package:fooduck/managers/food_data_manager.dart';
-import 'package:fooduck/pages/homepage.dart';
+import 'package:fooduck/pages/splash_screen.dart';
 
 Future<void> main() async{
   runApp(const MainApp());
@@ -19,7 +19,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData.from(
         colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 166, 0)),
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:fooduck/pages/homepage.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({ super.key });
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Homepage(),));
+    });
+
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Align(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Image(image: ExactAssetImage("assets/icon.png"), width: 128,),
+            Text("FooDuck", style: TextStyle(color: Colors.white, fontSize: 30),),
+          ],
+        ),
+      ),
+    );
+  }
+}
