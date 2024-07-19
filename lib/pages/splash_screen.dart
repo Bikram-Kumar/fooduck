0e2068d8d543d9e7d687a505d970049bfedd3bfa
Bikram Kumar <coolbikram0@gmail.com>
2024-07-19 15:35:54 +0530

    reduced splash time to 3 and xml launcher bg to black

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -14,7 +14,7 @@ class SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
-    Timer(Duration(seconds: 5), () {
+    Timer(Duration(seconds: 3), () {
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Homepage(),));
     });
 
